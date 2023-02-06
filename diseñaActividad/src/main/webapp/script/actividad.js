46a2c8d567855da91994918a1845763f77313dea
woodes7 <115068112+woodes7@users.noreply.github.com>
2023-02-06 09:21:31 +0100

    no message

---
+++ diseñaActividad/src/main/webapp/script/actividad.js
@@ -0,0 +1,27 @@
+function primera(){
+
+let variable1 = 10;
+
+let variable2 = 5.3;
+
+document.write(typeof(variable1));
+
+document.write(typeof variable2);
+
+} 
+
+function segunda(){
+let numero1 = 16;
+
+let numero2 = 29;
+
+let resultadoOperacion = numero1 + numero2;
+
+console.log(resultadoOperacion);
+}
+
+function tercera(){
+	let operacion = 46 +(8 * 2 / 2) + 46;
+
+console.log(operacion);	
+}
